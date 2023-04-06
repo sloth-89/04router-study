fea42ff1d1daa759b5fa0a6c3ba81cee1e56ed68
restvoice2020@gmail.com <restvoice2020@gmail.com>
2023-04-06 17:43:18 +0900

    router-study start

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 // html, css, js = 예전 방식
 // SPA = 요즘 방식
 // Single Page Application = react, vue, angular
+// 과거에는 noe-SPA: 서버가 라우팅 담당 (백엔드)
+// 최근은 SPA:클라이언트가 라우팅 담당 (프론트엔드)
 
-import logo from './logo.svg';
-import './App.css';
+// CSR = Client-side redering
+// SSR = Sever-side rendering
 
-function App() {
+import React from "react";
+import About from "./About";
+import Home from "./Home";
+import {Route, Routes} from "react-router-dom";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home/>}/> {/* /을 입력하면 localhost:3000과 같다. */}
+        <Route path="/about" element={<About/>}/>
+      </Routes>
     </div>
   );
 }
